918aa73129be7e7650f1272e5782a24dfc12fd13
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-08 22:00:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -64,3 +64,8 @@ def monopoly_with_fourth_tier
 	 }
 	}
 end
+
+names["reading_railroad"] = "mortgage_value"=>"$100"
+names["pennsylvania_railroad"] = mortgage_value"=>"$200"
+names["b_and_o_railroad"] = "mortgage_value"=>"$400"
+names["shortline"] ="mortgage_value"=>"$800"
