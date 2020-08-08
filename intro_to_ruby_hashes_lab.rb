ce150dd8473d9691a4b424b779631fd6cb781f8c
Aaron Goen <aarongoen2.0@gmail.com>
2020-08-08 22:33:04 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -56,6 +56,13 @@ def monopoly_with_fourth_tier
   :railroads=>
 	 {
 	  :pieces => 4,
+	  :rent_in_dollars => 
+	  {
+	    :one_piece_owned=>25,
+	    :two_pieces_owned=>50,
+	    :three_pieces_owned=>100,
+	    :four_pieces_owned=>200
+	 },
 	  :names => 
 	  {
 	    :reading_railroad=>
@@ -74,15 +81,7 @@ def monopoly_with_fourth_tier
 	    {
         "mortgage_value"=>"$800"
       }
-	  },
-	  :rent_in_dollars => 
-	  {
-	    :one_piece_owned=>25,
-	    :two_pieces_owned=>50,
-	    :three_pieces_owned=>100,
-	    :four_pieces_owned=>200
 	  }
 	 }
 	}
-}
 end
